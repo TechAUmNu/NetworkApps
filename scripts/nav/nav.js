c76cf0c79599095f1530bdd8ad1f4a1681a56206
Adam <astrutt@hotmail.co.uk>
2017-02-23 16:39:01 +0000

    Half working side bar menu

--- scripts/nav/nav.js
+++ scripts/nav/nav.js
@@ -24,3 +24,22 @@ function dragstart(e) {
     source = e.target;
     e.dataTransfer.effectAllowed = 'move';
 }
+
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
