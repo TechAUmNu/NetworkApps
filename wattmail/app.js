bccc396d2120a1f18e93f3960c4130bb59e937b1
Adam Paul Strutt <astrutt@hotmail.co.uk>
2017-03-27 17:22:12 +0100

    Login working

--- wattmail/app.js
+++ wattmail/app.js
@@ -13,7 +13,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/nodejs-game');
+mongoose.connect("mongodb://localhost/watt");
 var db = mongoose.connection;
 
 var app = express();
