93db4ba6c0d12cc206a5cfd46a350b184fce5870
Duncan Robertson <dar30@linux42.macs.hw.ac.uk>
2017-03-30 13:58:16 +0100

    Fixed input fields on register; fixed fonts; fixed formatting on register.

--- wattmail/app.js
+++ wattmail/app.js
@@ -13,8 +13,8 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-// mongodb://localhost/watt
-mongoose.connect("// mongodb://localhost/watt");
+//mongodb://localhost/watt
+mongoose.connect("mongodb://localhost/watt");
 var db = mongoose.connection;
 
 var app = express();
